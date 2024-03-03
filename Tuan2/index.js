d372d4cfa9030b9b9b46c5c858ae6eaff25370e4
hbngocc <hobaongoc2016@gmail.com>
2024-03-03 08:12:52 +0700

    btvn

--- Tuan2/index.js
+++ Tuan2/index.js
@@ -2,15 +2,69 @@ import { auth, dangky, dangnhap } from "./firebaseConfig.js";
 
 const loginBtn = document.querySelector("#btnLogin");
 
-loginBtn.addEventListener("click", async function () {
-    let email = document.querySelector("#form2Example17");
-    let pass = document.querySelector("#form2Example27");
-    const { isSuccess, infoMessage } = await dangky(auth, email.value, pass.value);
-    if (isSuccess) {
-        alert("Dang ky thanh cong");
+let emailInput = document.querySelector("#email");
+let passwordInput = document.querySelector("#pass");
+let btnLogin = document.querySelector(".btnLogin");
+let errorMessages = document.querySelectorAll(".errorMessage");
+let errorMessageEmail = errorMessages[0];
+let errorMessagePassword = errorMessages[1];
+
+
+// Đặt sự kiện click vào nút login
+btnLogin.addEventListener("click", function () {
+  let emailValue = emailInput.value;
+  let passValue = passwordInput.value;
+
+  // Kiểm tra mật khẩu và tài khoản đã rỗng chưa
+  if (emailValue !== "" && passValue !== "") {
+    // email và mật khẩu đều không rỗng
+    // Reset CSS và text khi không rỗng
+    emailInput.classList.remove("error");
+    passwordInput.classList.remove("error");
+    errorMessageEmail.innerText = "";
+    errorMessagePassword.innerText = "";
+
+    // Kiểm tra tiếp email không rỗng và pass không rỗng
+    // Kiểm tra email và pass có đúng hay không ?
+    if (emailValue == account.email && passValue == account.pass) {
+      // EMAIL VÀ MẬT KHẨU CHÍNH XÁC
+      // Đăng nhập thành công
+      alert("Đăng nhập thành công");
+      localStorage.setItem("dangnhap", account.email);
+      location.href = "index.html";
+    } else if (emailValue == account.email && passValue !== account.pass) {
+      emailInput.classList.remove("error");
+      passwordInput.classList.add("error");
+      errorMessageEmail.innerText = "";
+      errorMessagePassword.innerText = "Sai mật khẩu";
+    } else if (emailValue !== account.email && passValue == account.pass) {
+      emailInput.classList.add("error");
+      passwordInput.classList.remove("error");
+      errorMessageEmail.innerText = "Sai tài khoản";
+      errorMessagePassword.innerText = "";
     } else {
-        if(infoMessage == "auth/missing-password") {
-            pass.classList.add("errorPass");
-        }
+      emailInput.classList.add("error");
+      passwordInput.classList.add("error");
+      errorMessageEmail.innerText = "Sai tài khoản";
+      errorMessagePassword.innerText = "Sai mật khẩu";
     }
+  } else if (emailValue == "" && passValue !== "") {
+    // Email rỗng và pass không rỗng
+    emailInput.classList.add("error");
+    passwordInput.classList.remove("error");
+    errorMessageEmail.innerText = "Email Error";
+    errorMessagePassword.innerText = "";
+  } else if (emailValue !== "" && passValue == "") {
+    // Email không rỗng và pass lại rỗng
+    emailInput.classList.remove("error");
+    passwordInput.classList.add("error");
+    errorMessageEmail.innerText = "";
+    errorMessagePassword.innerText = "Password Error";
+  } else {
+    // Email và pass đều rỗng
+    emailInput.classList.add("error");
+    passwordInput.classList.add("error");
+    errorMessageEmail.innerText = "Tài khoản rỗng";
+    errorMessagePassword.innerText = "Mật khẩu rỗng";
+  }
 });
